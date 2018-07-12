03df7bbc20e90f19a54a870b6066c90ca47fa193
Samarth Gupta <samarthgupta1011@gmail.com>
2018-07-13 04:30:57 +0530

    Added admin apis

--- routes/globalAccess.js
+++ routes/globalAccess.js
@@ -8,14 +8,14 @@ admin.set('9999999999', true);
 
 
 
-module.exports = {
-    baseUrl : 'mongodb://localhost/SfaDb',
-    dbName : 'SfaDb',
-    admin : admin
-};
-
 // module.exports = {
-//     baseUrl : 'mongodb://samarthgupta1011:samarthgupta@ds121225.mlab.com:21225/sfadb',
-//     dbName : 'sfadb',
+//     baseUrl : 'mongodb://localhost/SfaDb',
+//     dbName : 'SfaDb',
 //     admin : admin
 // };
+
+module.exports = {
+    baseUrl : 'mongodb://samarthgupta1011:samarthgupta@ds121225.mlab.com:21225/sfadb',
+    dbName : 'sfadb',
+    admin : admin
+};
